cccba40d3713d53e9217967d9def7a9ef7d9822c
“maitreverge_42” <verge.f@gmail.com>
2024-07-09 10:57:07 +0200

    push websites

---
+++ script_hack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# URL of the website to be opened
+URL="https://fake-updates.com/hacker"
+
+# Command to launch Chrome in kiosk mode, which is a full-screen mode
+googlechrome --kiosk "$URL"
+
